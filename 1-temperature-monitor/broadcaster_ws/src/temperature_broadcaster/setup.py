b910d0c85162f1e3b8835cf12f7e3cc769e47bb0
Nathan Romany <91500@must.edu.eg>
2025-08-05 02:33:17 +0300

    Add testing nodes

--- 1-temperature-monitor/broadcaster_ws/src/temperature_broadcaster/setup.py
+++ 1-temperature-monitor/broadcaster_ws/src/temperature_broadcaster/setup.py
@@ -1,25 +1,24 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'temperature_broadcaster'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='nathan',
-    maintainer_email='91500@must.edu.eg',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer_email='nathan@example.com',
+    description='Test broadcaster node',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'test_broadcaster_node = temperature_broadcaster.test_broadcaster_node:main',
         ],
     },
 )
