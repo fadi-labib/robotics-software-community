578459451aed155f6210d19fe5bde01233faa2bf
Youhana Beshay <youhanabeshay@gmail.com>
2025-08-19 01:04:25 +0300

    Update license,email,description and version

--- 1-temperature-monitor/packages/temperature_pub/setup.py
+++ 1-temperature-monitor/packages/temperature_pub/setup.py
@@ -4,7 +4,7 @@ package_name = 'temperature_pub'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,10 +13,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='youhana',
-    maintainer_email='youhana@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Youhana Beshay',
+    maintainer_email='youhanabeshay@gmail.com',
+    description='Package containing a publisher that simulates temperature readings from sensors',
+    license='MPL-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
